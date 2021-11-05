e40330e2b6e27bf8aff654f3180afe7579da6670
mac <847118663@qq.com>
2021-11-05 12:49:50 +0800

    ws 获取session信息原码级解读， 详情请看tomcat 原码学习

--- src/main/java/top/piao888/springboot/config/HttpSessionConfigurator.java
+++ src/main/java/top/piao888/springboot/config/HttpSessionConfigurator.java
@@ -8,6 +8,13 @@ import javax.websocket.server.ServerEndpointConfig.Configurator;
 
 /**
  * 配置 将httpsession 放入到getUserProperties map中去
+ * WsHttpUpgradeHandler 执行init方法的时候
+ * ServerEndpointConfig 就已经被初始化和赋值完成了
+ *
+ * 而Configurator  类 是ServerEndpointConfig 接口的一个 成员变量
+ * 很显然HttpSessionConfigurator此时此刻 也是 ServerEndpointConfig的一个成员变量。
+ *
+ * 因为ServerEndpointConfig 在WsHttpUpgradeHandler.init时已经初始完毕了，所以相应的modifyHandshake 也已经执行，session已经被获取下来
  */
 public class HttpSessionConfigurator extends Configurator {
  
